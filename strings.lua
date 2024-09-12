1872c0e961efab0c49ef39a38abfdcc8ac9fdf7c
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-09-12 21:27:01 +0200

    BR 248 014 Northrail neu

--- strings.lua
+++ strings.lua
@@ -42,6 +42,8 @@ return {
 		["br247_desc"] = 			"Der Vectron DE xxxxxxxxxxxxxxx",
 		
 		["br248_siemens_name"] = 	"BR 248 001 Siemens (2019)",
+		["br248_rp_name"] = 		"BR 248 001 RP (2021)",
+		["br248_north_name"] = 		"BR 248 014 Northrail (2021)",
         ["br248_menu"] = 			"Vectron BR 248 DM",
 		["br248_desc"] = 			"Der Vectron DM xxxxxxxxxxxxxxx",		
 		
