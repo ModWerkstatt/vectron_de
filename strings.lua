036113b4ad27f19248f4e6d4ed137fd0425c9a51
Florian Thiel <flth@flth.de>
2024-09-18 21:31:58 +0200

    Strings angepasst

--- strings.lua
+++ strings.lua
@@ -2,34 +2,89 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "Siemens Vectron MS and AC",
-		["mod_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. The Vectron can operate in 20 European countries. The Vectron was first presented to the public in 2010, and since then the locomotive has been in use on many state and private railways. The package contains the locomotives class 193 MS (multi-system), class 193 AC (alternating current) and the Smartron.",
+		["mod_name"] = "Siemens Vectron DE, DM and DML",
+		["mod_desc"] = "After the Vectron has enjoyed growing popularity since 2010, further variants were introduced in 2018. These are a dual-power locomotive (Dual Mode) and a Dual Mode light, which is intended for light goods train services and train formation tasks. These locomotives have significantly lower performance values than the electric Vectron. Both versions are included in the mod for both electric and diesel operation.\nThe mod also includes the purely diesel-electric version of the Vectron.",
 
-        ["br193_ms_menu"] = "Vectron class 193 MS",
-		["br193_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. In 2014 the Vectron MS was presented for the first time with a speed of 200 km/h (also available with 160 km/h) and a continuous output of 6400 kW.",
+		["br247_de"] = "Activate Vectron DE Class 247?",
+		["option_br247_desc"] = "Should the Vectron DE (diesel-electric) Class 247 be activated?",
+		["br248_dm"] = "Activate Vectron DM Class 248?",
+		["option_br248_desc"] = "Should the Vectron DM (Dual Mode) Class 248 be activated?",
+		["br249_dml"] = "Activate Vectron DML Clas 249 DML?",
+		["option_br249_desc"] = "Should the Vectron (Dual Mode light) Class 249 be activated?",
 
-		["br192_menu"] = "Smartron class 192",
-		["br192_fake_menu"] = "Smartron class 192",
-		["br192_desc"] = "The Smartron is a slimmed down version of the Vectron. The Smartron is only available in blue metallic, the power is 5600 kW and is only approved for 140 km/h, the locomotive only runs in one country at a time. Smartron has the train protection systems PZB/LZB and ETCS for Germany.",
+		["diesel_betrieb"] = "Activate Vectron in diesel mode?",
+		["option_diesel_desc"] = "Should the Vectron in diesel mode be activated?",
+		["elektrischer_betrieb"] = "Activate Vectron in electrical mode?",
+		["option_elektrisch_desc"] = "Should the Vectron be activated in electric mode?",
+
+
+
+		["br247_siemens_name"] = 	"Class 247 901 Siemens (2014)",
+		["br247_db_name"] = 		"Class 247 906 DB Cargo (2017)",
+		["br247_rdc_name"] = 		"Class 247 909 RDC Anne (2018)",
+		["br247_rdc2_name"] = 		"Class 247 908 RDC/Siemens Debbie (2017)",
+		["br247_rts_name"] = 		"Class 247 906 RTS (2019)",
+        ["br247_menu"] = 			"Vectron Class 247 DE",
+		["br247_desc"] = 			"The diesel-electric version of the Vectron is abbreviated to DE and in Germany bears the series designation 247.",
+
+		["br248_siemens_name"] = 	"Class 248 001 Siemens (2019)",
+		["br248_rp_name"] = 		"Class 248 001 RP (2021)",
+		["br248_north_name"] = 		"Class 248 014 Northrail (2021)",
+		["br248_north2_name"] = 	"Class 248 021 Northrail (2022)",
+		["br248_north3_name"] = 	"Class 248 046 Northrail (2023)",
+		["br248_north4_name"] = 	"Class 248 075 Northrail (2023)",
+		["br248_cap_name"] = 		"Class 248 010 Captrain (2021)",
+		["br248_cap2_name"] = 		"Class 248 037 Captrain (2024)",
+		["br248_bm_name"] = 		"Class 248 011 BM Bahndienste (2021)",
+		["br248_locon_name"] = 		"Class 248 013 Locon (2021)",
+		["br248_locon2_name"] = 	"Class 248 047 Locon (2023)",
+		["br248_rhc_name"] = 		"Class 248 015 RheinCargo (2022)",
+		["br248_rhc2_name"] = 		"Class 248 057 RheinCargo (2023)",
+		["br248_tri_name"] = 		"Class 248 018 Triangula (2022)",
+		["br248_stern_name"] = 		"Class 248 996 Stern & Hafferl (2022)",
+		["br248_press_name"] = 		"Class 248 026 Press (2022)",
+		["br248_alpha_name"] = 		"Class 248 036 Alpha Trains (2022)",
+		["br248_arcelor_name"] = 	"Class 248 042 ArcelorMittal (2023)",
+		["br248_skl_name"] = 		"Class 248 023 SKL (2023)",
+		["br248_egp_name"] = 		"Class 248 042 EGP/Bahnoperator (2023)",
+		["br248_duisport_name"] = 	"Class 248 074 duisport rail (2023)",
+		["br248_cd_name"] = 		"Class 248 082 CD Cargo (2024)",
+		["br248_erc_name"] = 		"Class 248 039 ERC (2023)",
+		["br248_hrs_name"] = 		"Class 248 048 Hamburger Rail Service (2023)",
+		["br248_leonhard_name"] = 	"Class 248 040 Leonhard Weiss (2023)",
+		["br248_db_name"] = 		"Class 248 500 DB Fernverkehr - currently fictitious (2025)",
+		["br248_mkb_name"] = 		"Class 248 005 Mindener Kreisbahnen (2021)",
+        ["br248_menu"] = 			"Vectron Class 248 DM",
+		["br248_desc"] = 			"Dual Mode, or DM for short, is the name given to the dual-power variant of the Vectron. In Germany, it operates under the series designation 248.",
+
+		["br249_db_name"] = 		"Class 249 004 DB Cargo with RK 900 (2022)",
+		["br249_db2_name"] = 		"Class 249 003 DB Cargo - Das ist Grün with RK 900 (2022)",
+		["br249_db3_name"] = 		"Class 249 012 DB Cargo (2024)",
+        ["br249_menu"] = 			"Vectron Class 249 DML",
+		["br249_desc"] = 			"The abbreviation DML stands for Dual Mode light, a light dual-power version of the Vectron. It is primarily intended for light goods trains and shunting operations. In Germany it has the series designation 249.",
+
+        -- Fake
+        ["Fake_br247"] = "Activate fake locomotives?",
+        ["option_fake_br247_desc"] = "Should the fake locomotives (pantos down, engine off, no lights, no driver) be activated?",
 
 
 	},
 	de = {
 
 		["mod_name"] = "Siemens Vectron DE, DM und DML",
-		["mod_desc"] = "Der Vectron    ",
+		["mod_desc"] = "Nachdem der Vectron seit 2010 sich wachsender Beliebtheit erfreut, wurde 2018 weitere Varianten vorgestellt. Es handelt sich dabei um eine Zweikraftlokomotive (Dual Mode) und eine Dual Mode light, die für den leichten Güterzugdienst und Zugbildungsaufgaben vorgesehen ist. Diese Loks haben wesentlich geringere Leistungswerte als der elektrische Vectron. Beide Versionen sind sowohl für den elektrischen als auch den Dieselbetrieb im Mod enthalten.\nDesweiteren ist im Mod die rein dieselelektrische Variante des Vectron enthalten.",
 
 		["br247_de"] = "Vectron DE BR 247 aktivieren?",
-		["option_br247_desc"] = "Soll der Vectron DE BR 247 aktiviert werden?",
+		["option_br247_desc"] = "Sollen die Vectron DE (dieselelektrisch) BR 247 aktiviert werden?",
 		["br248_dm"] = "Vectron DM BR 248 aktivieren?",
-		["option_br248_desc"] = "Soll der Vectron DM BR 248 aktiviert werden?",
-		["br249_dml"] = "Vectron DML BR 249 aktivieren?",
-		["option_br249_desc"] = "Soll der Vectron DE BR 249 aktiviert werden?",
-		
-		["diesel_betrieb"] = "Vectron im Diesel Betrieb aktivieren?",
-		["option_diesel_desc"] = "Soll der Vectron im Diesel Betirbe aktiviert werden?",
-		["elektrischer_betrieb"] = "Vectron im Elektrischen Betrieb aktivieren?",
-		["option_elektrisch_desc"] = "Soll der Vectron im Elektrischen Betrieb aktiviert werden?",
+		["option_br248_desc"] = "Sollen die Vectron DM (Dual Mode) BR 248 aktiviert werden?",
+		["br249_dml"] = "Vectron DML BR 249 DML aktivieren?",
+		["option_br249_desc"] = "Sollen die Vectron (Dual Mode light) BR 249 aktiviert werden?",
+
+		["diesel_betrieb"] = "Vectron im Dieselbetrieb aktivieren?",
+		["option_diesel_desc"] = "Sollen die Vectron im Dieselbetrieb aktiviert werden?",
+		["elektrischer_betrieb"] = "Vectron im elektrischen Betrieb aktivieren?",
+		["option_elektrisch_desc"] = "Sollen die Vectron im elektrischen Betrieb aktiviert werden?",
 
 
 
@@ -39,8 +94,8 @@ return {
 		["br247_rdc2_name"] = 		"BR 247 908 RDC/Siemens Debbie (2017)",
 		["br247_rts_name"] = 		"BR 247 906 RTS (2019)",
         ["br247_menu"] = 			"Vectron BR 247 DE",
-		["br247_desc"] = 			"Der Vectron DE xxxxxxxxxxxxxxx",
-		
+		["br247_desc"] = 			"Die dieselelektrische Version des Vectron wird abgekürzt mit DE und trägt in Deutschland die Baureihenbezeichnung 247.",
+
 		["br248_siemens_name"] = 	"BR 248 001 Siemens (2019)",
 		["br248_rp_name"] = 		"BR 248 001 RP (2021)",
 		["br248_north_name"] = 		"BR 248 014 Northrail (2021)",
@@ -66,22 +121,20 @@ return {
 		["br248_erc_name"] = 		"BR 248 039 ERC (2023)",
 		["br248_hrs_name"] = 		"BR 248 048 Hamburger Rail Service (2023)",
 		["br248_leonhard_name"] = 	"BR 248 040 Leonhard Weiss (2023)",
-		["br248_db_name"] = 		"BR 248 500 DB Fernverkehr - Aktuell Fiktiv (2025)",
-		["br248_mkb_name"] = 		"BR 248 005 Minden Kreisbahn (2021)",
+		["br248_db_name"] = 		"BR 248 500 DB Fernverkehr - aktuell fiktiv (2025)",
+		["br248_mkb_name"] = 		"BR 248 005 Mindener Kreisbahnen (2021)",
         ["br248_menu"] = 			"Vectron BR 248 DM",
-		["br248_desc"] = 			"Der Vectron DM xxxxxxxxxxxxxxx",		
-		
+		["br248_desc"] = 			"Als Dual Mode, oder kurz DM, wird die Zweikraftvariante des Vectron bezeichnet. In Deutschland verkehrt sie unter der Baureihenbezeichnung 248.",
+
 		["br249_db_name"] = 		"BR 249 004 DB Cargo mit RK 900 (2022)",
 		["br249_db2_name"] = 		"BR 249 003 DB Cargo - Das ist Grün mit RK 900 (2022)",
 		["br249_db3_name"] = 		"BR 249 012 DB Cargo (2024)",
         ["br249_menu"] = 			"Vectron BR 249 DML",
-		["br249_desc"] = 			"Der Vectron DML xxxxxxxxxxxxxx",
+		["br249_desc"] = 			"Unter der Abkürzung DML versteht man die Dual Mode light, eine leichte Zweikraftvariante des Vectron. Sie ist vor allem für leichte Güterzüge und den Rangierbetrieb gedacht. In Deutschland trägt sie die Baureihenbezeichnung 249.",
 
         -- Fake
         ["Fake_br247"] = "Fake-Loks aktivieren?",
-        ["option_fake_br247_desc"] = "Die Fake-Loks (Pantos unten, Motor aus, kein Licht, kein Fahrer) aktiviert werden?",
-
-
+        ["option_fake_br247_desc"] = "Sollen die Fake-Loks (Pantos unten, Motor aus, kein Licht, kein Fahrer) aktiviert werden?",
 
 	},
 }
